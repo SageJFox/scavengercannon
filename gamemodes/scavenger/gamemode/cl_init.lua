7dc107a9fb7d627540d6cd72d7720660264668ce
Sierra Foxtrot <sagejfox@aol.com>
2026-03-28 16:42:20 -0700

    cl_init.lua localization

--- gamemodes/scavenger/gamemode/cl_init.lua
+++ gamemodes/scavenger/gamemode/cl_init.lua
@@ -12,25 +12,27 @@ function GM:ShowMenuInstructions()
 		panel:SetFont("DebugFixed")
 		panel:SetAlignment(TEXT_ALIGN_CENTER)
 		panel:SetAutoStretchVertical(false)
-		panel:SetText("Hold down the Spawn Menu key (default 'Q') to open the menu and join the game.")
+		local menukey = input.LookupBinding("+menu")
+		panel:SetText(ScavLocalize("scav.menu.instruction", menukey and string.upper(menukey) or ScavLocalize("scav.nokey")))
+		
 		--panel:SizeToContents()
 		panel:InvalidateLayout()
 		panel:SizeToContents()
-		panel:SetPos(ScrW()-panel:GetWide()-32,ScrH()-panel:GetTall()-32)
+		panel:SetPos(ScrW() - panel:GetWide() - 32, ScrH() - panel:GetTall() - 32)
 		self:RemoveMenuInstructions()
 		self.MenuInstructionsPanel = panel
 	end
-	hook.Add("OnSpawnMenuOpen","HideMenuInstructions",hideinstructions)
+	hook.Add("OnSpawnMenuOpen", "HideMenuInstructions", hideinstructions)
 end
 
 function GM:RemoveMenuInstructions()
 	if IsValid(self.MenuInstructionsPanel) then
 		self.MenuInstructionsPanel:Remove()
 	end
-	--hook.Remove("OnSpawnMenuOpen","HideMenuInstructions")
+	--hook.Remove("OnSpawnMenuOpen", "HideMenuInstructions")
 end
 
-hook.Add("InitPostEntity","SetupScoreboard",function()
+hook.Add("InitPostEntity", "SetupScoreboard", function()
 
 	GAMEMODE.ScoreBoard = vgui.Create("sdm_sb_scoreboard")
 
@@ -71,75 +73,73 @@ function GM:SpawnMenuEnabled()
 end
 
 function GM:OnSpawnMenuOpen()
-	if IsValid(self.Menu) then
-		self.Menu:SetVisible(true)
-		self.Menu:MakePopup()
-		self.Menu:SetKeyboardInputEnabled(false)
-	end
+	if not IsValid(self.Menu) then return end
+
+	self.Menu:SetVisible(true)
+	self.Menu:MakePopup()
+	self.Menu:SetKeyboardInputEnabled(false)
 end
 
 function GM:OnSpawnMenuClose()
-	if IsValid(self.Menu) then
-		self.Menu:SetVisible(false)
-	end
+	if not IsValid(self.Menu) then return end
+
+	self.Menu:SetVisible(false)
 end
 
 local last_movez = 0
 local last_landz = 0
 local sprint_ang = 0
 
-function GM:CalcView(pl,pos,angles,fov)
-	local tab = self.BaseClass:CalcView(pl,pos,angles,fov)
-	if pl.landingtime and CurTime()-pl.landingtime < math.Min(math.sqrt(pl.landingspeed)/35,1) then
-		local progress = math.sqrt(math.Clamp((CurTime()-pl.landingtime)/math.Min(math.sqrt(pl.landingspeed)/35,1),0,1))
-		last_landz = math.sin(progress*math.pi)*math.sqrt(pl.landingspeed)/13
-		tab.origin.z = tab.origin.z-last_landz*0.7
+function GM:CalcView(pl, pos, angles, fov)
+	local tab = self.BaseClass:CalcView(pl, pos, angles, fov)
+	if pl.landingtime and CurTime() - pl.landingtime < math.Min(math.sqrt(pl.landingspeed) / 35, 1) then
+		local progress = math.sqrt(math.Clamp((CurTime() - pl.landingtime) / math.Min(math.sqrt(pl.landingspeed) / 35, 1), 0, 1))
+		last_landz = math.sin(progress * math.pi) * math.sqrt(pl.landingspeed) / 13
+		tab.origin.z = tab.origin.z - last_landz * 0.7
 	else
 		last_landz = 0
 	end
 	local pl_z = pl:GetVelocity().z
-	local sign = 1
-	if pl_z < 0 then
-		sign = -1
-	end
-	last_movez=math.Approach(last_movez,math.sqrt(math.abs(pl_z))/34*sign,10*FrameTime())
+	local sign =pl_z < 0 and -1 or 1
+
+	last_movez = math.Approach(last_movez, math.sqrt(math.abs(pl_z)) / 34 * sign, 10 * FrameTime())
 	if tab.vm_origin then
-		tab.vm_origin.z = tab.vm_origin.z-math.Clamp(last_landz+last_movez,-3,3)
+		tab.vm_origin.z = tab.vm_origin.z - math.Clamp(last_landz + last_movez, -3, 3)
 	end
 	if pl.sprinting then
 		if IsValid(pl:GetActiveWeapon()) then
 			pl:GetActiveWeapon().BobScale = 4
 		end
-		sprint_ang = math.Approach(sprint_ang,-13,80*FrameTime())
+		sprint_ang = math.Approach(sprint_ang, -13, 80 * FrameTime())
 	else
 		if IsValid(pl:GetActiveWeapon()) then
 			pl:GetActiveWeapon().BobScale = 1
 		end
-		sprint_ang = math.Approach(sprint_ang,0,80*FrameTime())
+		sprint_ang = math.Approach(sprint_ang, 0, 80 * FrameTime())
 	end
 	if tab.vm_angles then
-		tab.vm_angles:RotateAroundAxis(tab.vm_angles:Right(),sprint_ang)
+		tab.vm_angles:RotateAroundAxis(tab.vm_angles:Right(), sprint_ang)
 	end
 	return tab
 end
 
-usermessage.Hook("sdm_pldmged",function(um)
+usermessage.Hook("sdm_pldmged", function(um)
 	--um:ReadEntity():EmitSound("physics/plastic/plastic_box_impact_bullet5.wav")
 	surface.PlaySound("physics/plastic/plastic_box_impact_bullet5.wav")
 	surface.PlaySound("physics/plastic/plastic_box_impact_bullet5.wav")
 end)
 
 
-local function getweapontoswitchto(pl,slot)
+local function getweapontoswitchto(pl, slot)
 	local tab = {}
-	for k,v in pairs(pl:GetWeapons()) do
-		if v.Slot == slot then
-			table.insert(tab,v)
-		end
+	for _, v in pairs(pl:GetWeapons()) do
+		if v.Slot ~= slot then continue end
+
+		table.insert(tab, v)
 	end
 	local wep = pl:GetActiveWeapon()
-	table.SortByMember(tab,"SlotPos")
-	for k,v in pairs(tab) do
+	table.SortByMember(tab, "SlotPos")
+	for _, v in pairs(tab) do
 		if nextiswep then
 			wep = v
 			break
@@ -162,13 +162,13 @@ local slottranslate = {}
 	slottranslate["slot8"] = 7
 	slottranslate["slot9"] = 8
 
-function GM:PlayerBindPress(pl,bind,pressed)
-	if slottranslate[bind] then
-		local wep = getweapontoswitchto(pl,slottranslate[bind])
-		if IsValid(wep) then
-			RunConsoleCommand("use",wep:GetClass())
-		end
-		return true
+function GM:PlayerBindPress(pl, bind, pressed)
+	if not slottranslate[bind] then return false end
+
+	local wep = getweapontoswitchto(pl, slottranslate[bind])
+	if IsValid(wep) then
+		RunConsoleCommand("use", wep:GetClass())
 	end
-	return false
+
+	return true
 end
