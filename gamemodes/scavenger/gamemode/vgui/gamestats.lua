9d300835db2de65c773351321176d2add5bb0b1e
Sierra Foxtrot <sagejfox@aol.com>
2026-03-28 16:07:22 -0700

    gamestats and teamsmenu.lua localization
    All existing strings in vgui folder should be handled now

--- gamemodes/scavenger/gamemode/vgui/gamestats.lua
+++ gamemodes/scavenger/gamemode/vgui/gamestats.lua
@@ -1,71 +1,67 @@
 
 local PANEL = {}
 	function PANEL:Init()
-		self:SetTitle("Round Winner")
-		self.NameLabel = vgui.Create("DLabel",self)
+		self:SetTitle("#scav.round.winner.player")
+		self.NameLabel = vgui.Create("DLabel", self)
 			self.NameLabel:SetFont("DermaLarge")
 		self.initialized = true
 	end
 	
 	function PANEL:InvalidateLayout()
-		if not self.initalized then
-			return
-		end
+		if not self.initalized then return end
 	end
 	
 	function PANEL:SetPlayer(pl)
-		if self.Icon then
-			self.Icon:Remove()
-		end
-		self.Icon = vgui.Create("AvatarImage",self)
+		if self.Icon then self.Icon:Remove() end
+
+		self.Icon = vgui.Create("AvatarImage", self)
 		self.NameLabel:SetText(pl:Nick())
 		self.NameLabel:SizeToContents()
 		self.Icon:SetPlayer(pl)
 	end
 	
 	function PANEL:AutoSetup()
-		self:SetSize(430,180)
-		self.Icon:SetSize(128,128)
-		self.Icon:SetPos(16,40)
-		self.NameLabel:SetPos(150,32)
+		self:SetSize(430, 180)
+		self.Icon:SetSize(128, 128)
+		self.Icon:SetPos(16, 40)
+		self.NameLabel:SetPos(150, 32)
 	end
 	
 
-	vgui.Register("sdm_winnerpanel_pl",PANEL,"DFrame")
+	vgui.Register("sdm_winnerpanel_pl", PANEL, "DFrame")
 	
 local PANEL = {}
 	function PANEL:Init()
-		self:SetTitle("Round Winner")
-		self.NameLabel = vgui.Create("DLabel",self)
+		self:SetTitle("#scav.round.winner.team")
+		self.NameLabel = vgui.Create("DLabel", self)
 			self.NameLabel:SetFont("DermaLarge")
 		self.Initialized = true
-		self.TeamBox = vgui.Create("DPanelList",self)
+		self.TeamBox = vgui.Create("DPanelList", self)
 	end
 	
 	function PANEL:InvalidateLayout()
-		if not self.Initialized then
-			return
-		end
+		if not self.Initialized then return end
+
 		self.NameLabel:SizeToContents()
-		self.NameLabel:SetPos((self:GetWide()-self.NameLabel:GetWide())/2,32)
-		self.TeamBox:SetPos(16,self.NameLabel.y+self.NameLabel:GetTall()+16)
-		self.TeamBox:SetSize(self:GetWide()-32,self:GetTall()-self.TeamBox.y-16)
+		self.NameLabel:SetPos((self:GetWide() - self.NameLabel:GetWide()) / 2, 32)
+		self.TeamBox:SetPos(16, self.NameLabel.y + self.NameLabel:GetTall() + 16)
+		self.TeamBox:SetSize(self:GetWide() - 32, self:GetTall() - self.TeamBox.y - 16)
 		
 	end
 	
 	function PANEL:SetTeam(teamid)
-		self.NameLabel:SetText(team.GetName(teamid).." wins the round!")
+		self.NameLabel:SetText(ScavLocalize("scav.round.winner.announce", team.GetName(teamid)))
 		self.NameLabel:SizeToContents()
 		self.TeamBox:Clear()
 		local w = self:GetWide()
-		for _,pl in pairs(team.GetPlayers(teamid)) do
-			local panel = vgui.Create("sdm_winnerpanel_team_playerbox",self)
-			panel:SetSize(w,42)
+		for _, pl in pairs(team.GetPlayers(teamid)) do
+			local panel = vgui.Create("sdm_winnerpanel_team_playerbox", self)
+			panel:SetSize(w, 42)
 			panel:SetPlayer(pl)
 			self.TeamBox:AddItem(panel)
 		end
 		local col = table.Copy(team.GetColor(teamid))
-		local mul = GetConVarNumber("scav_col_team_mul")
+		local mul = 0.7 --GetConVarNumber("scav_col_team_mul")
 		col.r = col.r*mul
 		col.g = col.g*mul
 		col.b = col.b*mul
@@ -74,44 +70,43 @@ local PANEL = {}
 	end
 	
 	function PANEL:AutoSetup()
-		self:SetSize(480,380)
-		self:SetPos(ScrW()/2-self:GetWide()/2,ScrH()/2-self:GetTall()/2)
+		self:SetSize(480, 380)
+		self:SetPos(ScrW() / 2 - self:GetWide() / 2, ScrH() / 2 - self:GetTall() / 2)
 		self:SetSkin("sg_menu")
 		self:MakePopup()
 	end
 	
 
-	vgui.Register("sdm_winnerpanel_team",PANEL,"DFrame")
+	vgui.Register("sdm_winnerpanel_team", PANEL, "DFrame")
 	
 local PANEL = {}
 
 function PANEL:Init()
-	self.NameLabel = vgui.Create("DLabel",self)
+	self.NameLabel = vgui.Create("DLabel", self)
 		self.NameLabel:SetFont("HUDHintTextLarge")
-	self.ScoreLabel = vgui.Create("DLabel",self)
+	self.ScoreLabel = vgui.Create("DLabel", self)
 		self.ScoreLabel:SetFont("HUDHintTextLarge")
-	self.Icon = vgui.Create("AvatarImage",self)
+	self.Icon = vgui.Create("AvatarImage", self)
 	self.Initialized = true
 end
 
 function PANEL:SetPlayer(pl)
 	self.Icon:Remove()
-	self.Icon = vgui.Create("AvatarImage",self)
+	self.Icon = vgui.Create("AvatarImage", self)
 	self.Icon:SetPlayer(pl)
-	self.Icon:SetSize(32,32)
+	self.Icon:SetSize(32, 32)
 	self.NameLabel:SetText(pl:Nick())
-	self.ScoreLabel:SetText("score: "..pl:Frags())
+	self.ScoreLabel:SetText(ScavLocalize("scav.score", pl:Frags()))
 end
 
 function PANEL:InvalidateLayout()
-	if not self.Initialized then
-		return
-	end
+	if not self.Initialized then return end
+
 	self.NameLabel:SizeToContents()
-	self.NameLabel:SetPos(self.Icon.x+self.Icon:GetWide()+8,(self:GetTall()-self.NameLabel:GetTall())/2)
-	self.Icon:SetPos(12,5)
+	self.NameLabel:SetPos(self.Icon.x + self.Icon:GetWide() + 8, (self:GetTall() - self.NameLabel:GetTall()) / 2)
+	self.Icon:SetPos(12, 5)
 	self.ScoreLabel:SizeToContents()
-	self.ScoreLabel:SetPos(self:GetWide()-128,(self:GetTall()-self.ScoreLabel:GetTall())/2)
+	self.ScoreLabel:SetPos(self:GetWide() - 128, (self:GetTall() - self.ScoreLabel:GetTall()) / 2)
 end
 
-vgui.Register("sdm_winnerpanel_team_playerbox",PANEL,"DPanel")
+vgui.Register("sdm_winnerpanel_team_playerbox", PANEL, "DPanel")
--- gamemodes/scavenger/gamemode/vgui/teamsmenu.lua
+++ gamemodes/scavenger/gamemode/vgui/teamsmenu.lua
@@ -7,7 +7,8 @@ local PANEL = {}
 		self.team = teamid
 	end
 
-	local teamstostrings = {}
+	-- ??
+	--[[local teamstostrings = {}
 		teamstostrings[TEAM_UNASSIGNED] = "unassigned"
 		teamstostrings[TEAM_SPECTATOR] = "spectators"
 		teamstostrings[TEAM_RED] = "red"
@@ -17,59 +18,57 @@ local PANEL = {}
 		teamstostrings[TEAM_ORANGE] = "orange"
 		teamstostrings[TEAM_PURPLE] = "purple"
 		teamstostrings[TEAM_BROWN] = "brown"
-		teamstostrings[TEAM_TEAL] = "teal"	
+		teamstostrings[TEAM_TEAL] = "teal"]]
 		
 	function PANEL:DoClick()
-		RunConsoleCommand("changeteam",self.team)
+		RunConsoleCommand("changeteam", self.team)
 		self.Menu:Remove()
 		gui.EnableScreenClicker(false)
 	end
 
-	vgui.Register("sdm_teamjoinbutton",PANEL,"DButton")
+	vgui.Register("sdm_teamjoinbutton", PANEL, "DButton")
 
 local PANEL = {}
 	
 	function PANEL:Init()
 		self.teams = {}
-		self.Form = vgui.Create("DForm",self)
-		self:SetTitle("Select Team")
+		self.Form = vgui.Create("DForm", self)
+		self:SetTitle("#scav.team.select")
 		self.Form:SetName("")
 		self.initialized = true
-		for k,v in pairs(GAMEMODE.Teams) do
-			if v then
-				self:AddTeam(k)
-			end
+		for k, v in pairs(GAMEMODE.Teams) do
+			if not v then continue end
+			self:AddTeam(k)
 		end
 	end
 	
 	function PANEL:AutoSetup()
-		self:SetSize(300,260)
-		self:SetPos(ScrW()/2-self:GetWide()/2,ScrH()/2-self:GetTall()/2)
+		self:SetSize(300, 260)
+		self:SetPos(ScrW() / 2 - self:GetWide() / 2, ScrH() / 2 - self:GetTall() / 2)
 	end
 
 	function PANEL:AddTeam(teamid)
-		if not self.teams[teamid] then
-			local button = vgui.Create("sdm_teamjoinbutton",self.Form)
-			self.Form:AddItem(button)
-			self.teams[teamid] = button
-			button.ColorSquare = vgui.Create("DImage",button)
-			button.ColorSquare:SetImage("vgui/sgskin/c_holo")
-				button.ColorSquare:SetSize(12,12)
-				button.ColorSquare:SetPos(4,button:GetTall()/2-button.ColorSquare:GetTall()/2)
-				button.ColorSquare:SetImageColor(team.GetColor(teamid))
-				button.ColorSquare:SetVisible(true)
-			button.Menu = self
-			button:SetTeam(teamid)
-			button:SetText(team.GetName(teamid))
-		end
+		if self.teams[teamid] then return end
+
+		local button = vgui.Create("sdm_teamjoinbutton", self.Form)
+		self.Form:AddItem(button)
+		self.teams[teamid] = button
+		button.ColorSquare = vgui.Create("DImage", button)
+		button.ColorSquare:SetImage("vgui/sgskin/c_holo")
+			button.ColorSquare:SetSize(12, 12)
+			button.ColorSquare:SetPos(4, button:GetTall() / 2 - button.ColorSquare:GetTall() / 2)
+			button.ColorSquare:SetImageColor(team.GetColor(teamid))
+			button.ColorSquare:SetVisible(true)
+		button.Menu = self
+		button:SetTeam(teamid)
+		button:SetText(team.GetName(teamid))
 	end
 	
 	function PANEL:InvalidateLayout()
-		if not self.initialized then
-			return
-		end
-		self.Form:SetPos(4,21)
-		self.Form:SetSize(self:GetWide()-8,self:GetTall()-21)
+		if not self.initialized then return end
+
+		self.Form:SetPos(4, 21)
+		self.Form:SetSize(self:GetWide() - 8, self:GetTall() - 21)
 	end
 	
-	vgui.Register("sdm_teamjoinmenu",PANEL,"DFrame")
+	vgui.Register("sdm_teamjoinmenu", PANEL, "DFrame")
