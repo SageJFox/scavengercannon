3e0e870c327dccfd0892d786585c5b3b34b85ad5
Sierra Foxtrot <sagejfox@aol.com>
2026-03-29 01:26:02 -0700

    weapons.lua cleanup

--- gamemodes/scavenger/gamemode/weapons.lua
+++ gamemodes/scavenger/gamemode/weapons.lua
@@ -1,7 +1,7 @@
 local weps = {
-"weapon_backuppistol",
-"weapon_blackholegun",
-"weapon_alchemygun"
+	"weapon_backuppistol",
+	"weapon_blackholegun",
+	"weapon_alchemygun"
 }
 
 function GM:GetValidWeapons()
@@ -9,8 +9,8 @@ function GM:GetValidWeapons()
 end
 
 if CLIENT then
-	hook.Add("InitPostEntity","wepslots",function()
-		for k,v in pairs(weps) do
+	hook.Add("InitPostEntity", "wepslots", function()
+		for _, v in pairs(weps) do
 			local SWEP = weapons.GetStored(v)
 			SWEP.Slot = 1
 			SWEP.SlotPos = 0
@@ -19,11 +19,7 @@ if CLIENT then
 else
 	function GM:PlayerLoadout(pl)
 		local wep = string.lower(pl:GetInfo("sdm_w2"))
-		if table.HasValue(weps,wep) then
-			pl:Give(wep)
-		else
-			pl:Give("weapon_backuppistol")
-		end
+		pl:Give(table.HasValue(weps, wep) and wep or "weapon_backuppistol")
 		pl:Give("scav_gun")
 		pl:SelectWeapon("scav_gun")
 	end
