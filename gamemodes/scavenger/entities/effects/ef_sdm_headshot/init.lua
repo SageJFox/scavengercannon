1a9e6dad97ec23f4addbb01f481cf2db6e9cae5d
Sierra Foxtrot <sagejfox@aol.com>
2026-04-01 01:56:22 -0700

    Fixed (and localized) headshot effect

--- gamemodes/scavenger/entities/effects/ef_sdm_headshot/init.lua
+++ gamemodes/scavenger/entities/effects/ef_sdm_headshot/init.lua
@@ -1,15 +1,15 @@
 function EFFECT:Init(data)
 	self.Created = CurTime()
-	self:EmitSound("weapons/ar1/ar1_dist1.wav",100,100)
+	self:EmitSound("weapons/ar1/ar1_dist1.wav", 100, 100)
 	self:SetModel("models/Gibs/HGIBS.mdl")
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_VPHYSICS)
-	self:GetPhysicsObject():SetVelocity(Vector(math.Rand(-5,5),math.Rand(5,5),math.Rand(170,240)))
+	self:GetPhysicsObject():SetVelocity(Vector(math.Rand(-5, 5), math.Rand(5, 5), math.Rand(170, 240)))
 	self:GetPhysicsObject():SetMaterial("gmod_silent")
 end
 
 function EFFECT:Think()
-	if self.Created+1 < CurTime() then
+	if self.Created + 1 < CurTime() then
 		return false
 	end
 	return true
@@ -17,13 +17,13 @@ end
 
 function EFFECT:Render()
 	local pos = self:GetPos():ToScreen()
-	local progress = math.Clamp(CurTime()-self.Created,0,1)
+	local progress = math.Clamp(CurTime() - self.Created, 0, 1)
 	cam.Start2D()
-		surface.SetTextColor(255,255,255,(1-progress)*255)
-		surface.SetFont("ConsoleText")
-		local text = ("Headshot!")
+		surface.SetTextColor(255, 255, 255, (1 - progress) * 255)
+		surface.SetFont("Scav_ConsoleText")
+		local text = ("#scav.sdm.headshot")
 		local w,h = surface.GetTextSize(text)
-		surface.SetTextPos(pos.x-w/2,pos.y-progress*48)
+		surface.SetTextPos(pos.x - w/2 , pos.y - progress * 48)
 		surface.DrawText(text)
 	cam.End2D()
 	return true
